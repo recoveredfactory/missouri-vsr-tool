29e15252abb76e1b78962acf9a4af6de643c4738
David Eads <davideads@gmail.com>
2026-01-31 22:01:45 -0800

    Add Umami analytics script

--- packages/web/src/routes/+layout.server.ts
+++ packages/web/src/routes/+layout.server.ts
@@ -1,13 +1,17 @@
+import { env } from "$env/dynamic/private";
+
 export async function load({ fetch }) {
   const response = await fetch("/data/dist/agency_index.json");
 
   if (!response.ok) {
     return {
       agencies: [],
+      umamiWebsiteId: env.UMAMI_WEBSITE_ID ?? null,
     };
   }
 
   return {
     agencies: await response.json(),
+    umamiWebsiteId: env.UMAMI_WEBSITE_ID ?? null,
   };
 }
--- packages/web/src/routes/+layout.svelte
+++ packages/web/src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script>
   import "../app.css";
+
+  export let data;
 </script>
+
+<svelte:head>
+  {#if data?.umamiWebsiteId}
+    <script
+      defer
+      src="https://cloud.umami.is/script.js"
+      data-website-id={data.umamiWebsiteId}
+    ></script>
+  {/if}
+</svelte:head>
 
 <slot />
