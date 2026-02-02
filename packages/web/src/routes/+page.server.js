a0b48a65269bc0ba445854cf4697f9bb76675927
David Eads <davideads@gmail.com>
2026-02-02 01:47:38 -0800

    Bundle about-the-data markdown for deploy

--- packages/web/src/routes/+page.server.js
+++ packages/web/src/routes/+page.server.js
@@ -1,12 +1,10 @@
-import { readFileSync } from "fs";
 import { compile } from "mdsvex";
+import aboutMarkdown from "../../content/about-the-data.md?raw";
+
+const aboutDataHtmlPromise = compile(aboutMarkdown).then((compiled) => compiled.code);
 
 export async function load() {
-  const markdownPath = new URL("../../content/about-the-data.md", import.meta.url);
-  const markdown = readFileSync(markdownPath, "utf-8");
-  const compiled = await compile(markdown);
-
   return {
-    aboutDataHtml: compiled.code,
+    aboutDataHtml: await aboutDataHtmlPromise,
   };
 }
